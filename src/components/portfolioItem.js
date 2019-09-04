fd82fb3cd3c99f8c3e38ad0047010d4ec6481590
Louie F <fitz4492@gmail.com>
2019-09-03 23:30:09 -0500

    dark

--- src/components/portfolioItem.js
+++ src/components/portfolioItem.js
@@ -1,17 +1,21 @@
-import React from "react"
+import React from 'react';
 
 const PortfolioItem = ({ img, alt, href, github_href, title, description }) => (
   <div className="card bg-white workCard">
     <h3 className="workh3 text-center text-dark mb-0 mt-5">{title}</h3>
-    <a href={href} className="container imgContainer"  target="_blank">
+    <a href={href} className="container imgContainer" target="_blank">
       <img className="workImage" src={img} alt={alt} />
     </a>
 
-    <a href={github_href} className="WorkBtns btn btn-primary btn-sm w-1 mb-2"  target="_blank" >
+    <a
+      href={github_href}
+      className="WorkBtns text-dark btn btn-primary btn-sm w-1 mb-2"
+      target="_blank"
+    >
       GITHUB
     </a>
     <p className="px-2">{description}</p>
   </div>
-)
+);
 
-export default PortfolioItem
+export default PortfolioItem;
